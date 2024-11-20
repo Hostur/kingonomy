22e3a5d92c0476989927a33627f4c55c5ff753fb
Hostur <f.zielinski.gliwice@gmail.com>
2024-11-20 15:40:05 +0100

    Started working on Resources Service

--- KingonomyService/Services/ResourcesService.cs
+++ KingonomyService/Services/ResourcesService.cs
@@ -1,6 +1,49 @@
-﻿namespace KingonomyService.Services
+﻿using Kingonomy.Models;
+using KingonomyService.DB;
+using KingonomyService.DB.Queries;
+using KingonomyService.Utils;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace KingonomyService.Services
 {
-    public class ResourcesService
+    public sealed class ResourcesService
     {
+        private readonly DBProvider _dbProvider;
+        private readonly IDistributedCache _cache;
+        public ResourcesService(DBProvider dbProvider, IDistributedCache cache)
+        {
+            _dbProvider = dbProvider;
+            _cache = cache;
+        }
+
+        public async Task<ResourceModel[]> GetResources(int playerId)
+        {
+            string id = $"resources_{playerId}";
+            var result = await _cache.GetRecordAsync<ResourceModel[]>(id);
+            if (result == null)
+            {
+                var dbResult = await new SelectPlayerResourcesQuery(playerId).Execute();
+                result = dbResult.ToArray();
+                await _cache.SetRecordAsync(id, result, TimeSpan.MaxValue, TimeSpan.MaxValue);
+            }
+
+            return result;
+        }
+
+        public async Task<bool> AddPlayerResource(int playerId, string resourceId, float value)
+        {
+            bool result = await _dbProvider.AddPlayerResources(playerId, resourceId, value);
+            if (result)
+                await RefreshResourcesCache(playerId);
+
+            return result;
+        }
+
+        private async Task RefreshResourcesCache(int playerId)
+        {
+            string id = $"resources_{playerId}";
+            var dbResult = await new SelectPlayerResourcesQuery(playerId).Execute();
+            await _cache.SetRecordAsync(id, dbResult.ToArray(), TimeSpan.MaxValue, TimeSpan.MaxValue);
+        }
     }
 }
